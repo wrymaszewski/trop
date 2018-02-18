6e3df2dabfc439f21d46810f72218adfdaa9093a
mikolaj_klarzuk <mikolaj.klarzuk@gmail.com>
2018-02-18 16:51:50 +0100

    Routes styled

--- static/js/master.js
+++ static/js/master.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
+$( document ).ready(function(){
+    
     // jquery datatables
     $('.datatable').DataTable({
         "scrollY":        "400px",
         "scrollCollapse": true,
         "paging":         false
     });
+
+    // Climbing locations - show children
+    let link = $('a');
+    link.on('click', function() {
+        $(this).parent().next().children('li').toggle();
+
+    });    
 });
