6e06f71f5252e92e7c0ced9a9e5b047dd2a6a777
wrymaszewski <plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.com>
2018-02-25 17:53:57 +0100

    staticfiles collected

---
+++ staticfiles/js/master.js
@@ -0,0 +1,16 @@
+$( document ).ready(function(){
+
+    // jquery datatables
+    $('.datatable').DataTable({
+        "scrollY":        "400px",
+        "scrollCollapse": true,
+        "paging":         false
+    });
+
+    // Climbing locations - show children
+    let link = $('a');
+    link.on('click', function() {
+        $(this).parent().next().children('li').toggle();
+    });
+
+});
