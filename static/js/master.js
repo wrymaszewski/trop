79a7e904bc98096b2fea59d709dd8bad7e4eedfb
mikolaj_klarzuk <daxtersky@gmail.com>
2018-02-25 23:53:36 +0100

    First batch of styling fixes

--- static/js/master.js
+++ static/js/master.js
@@ -1,5 +1,8 @@
+$('.content').append('<h1>Hello stranger!</h1><h2>Please be patient and wait for our app to load</h2>');    
+console.log('loading');
 $( document ).ready(function(){
-
+    
+    
     // jquery datatables
     $('.datatable').DataTable({
         "scrollY":        "400px",
