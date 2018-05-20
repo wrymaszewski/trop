efa8f4e353e678c8a05e2ed660edc77608d38d0f
wrymaszewski <plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.com>
2018-05-20 20:58:27 +0200

    accounts lint

--- accounts/models.py
+++ accounts/models.py
@@ -4,20 +4,23 @@ from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 from django.urls import reverse_lazy
 from cloudinary.models import CloudinaryField
-# Create your models here.
+
 
 class User(auth.models.User, auth.models.PermissionsMixin):
     def __str__(self):
         return self.username
 
+
 User = get_user_model()
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length = 100)
-    avatar = CloudinaryField('avatar', default = 'user-placeholder-circle_o5pzxf.png',
-                null=True, blank=True)
+    last_name = models.CharField(max_length=100)
+    avatar = CloudinaryField('avatar',
+                             default='user-placeholder-circle_o5pzxf.png',
+                             null=True, blank=True)
     email = models.EmailField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     hidden = models.BooleanField(default=False)
@@ -30,9 +33,10 @@ class UserProfile(models.Model):
         self.last_name = self.last_name.capitalize()
         super().save(*args, **kwargs)
 
+
 class Group(models.Model):
-    name = models.CharField(max_length = 255, unique=True)
-    slug = models.SlugField(allow_unicode = True, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
     description = models.TextField(blank=True, default='')
     members = models.ManyToManyField(User, through='GroupMember')
 
@@ -50,10 +54,15 @@ class Group(models.Model):
     class Meta:
         ordering = ['name']
 
+
 class GroupMember(models.Model):
-    group = models.ForeignKey(Group, related_name='memberships', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='user_groups', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='memberships',
+                              on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_groups',
+                             on_delete=models.CASCADE)
+
     def __str__(self):
         return self.user.username
+
     class Meta:
         unique_together = ('group', 'user')
--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import (CreateView, UpdateView,
-                             ListView, DetailView, RedirectView)
+                                  ListView, DetailView, RedirectView)
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+
 from indoor import charts as indoor_charts
 from routes import charts as outdoor_charts
 from .models import Group, GroupMember, UserProfile
 from . import forms
-from django.contrib import messages
 from posts.models import Post, Comment
 
-from django.contrib.auth import get_user_model
 User = get_user_model()
 
-# Create your views here.
-class UserProfileRedirectView(LoginRequiredMixin,RedirectView):
-    # redirection to user_profile or new_profile if used did not create one before
+
+class UserProfileRedirectView(LoginRequiredMixin, RedirectView):
+    # redirection to user_profile or new_profile
     permanent = True
+
     def get_redirect_url(self, *args, **kwargs):
         if hasattr(self.request.user, 'userprofile'):
-            return reverse('accounts:user_profile', kwargs = {'username': self.request.user.username})
+            return reverse('accounts:user_profile',
+                           kwargs={'username': self.request.user.username})
         else:
             return reverse('accounts:new_profile')
 
-class UserProfileCreationRedirectView(LoginRequiredMixin,RedirectView):
+
+class UserProfileCreationRedirectView(LoginRequiredMixin, RedirectView):
     # redicecting to home of new_profile if user did not create one before
     permanent = True
+
     def get_redirect_url(self, *args, **kwargs):
         if hasattr(self.request.user, 'userprofile'):
             return reverse('home')
         else:
             return reverse('accounts:new_profile')
+
 
 @login_required
 def get_user_profile(request, username):
@@ -41,68 +48,76 @@ def get_user_profile(request, username):
     if hasattr(user, 'userprofile'):
         userprofile = user.userprofile
         context_dict = {
-                        "userprofile": userprofile,
-                        "chart_list": [
-                                        indoor_charts.training_line_chart(username),
-                                        indoor_charts.gym_pie_chart(username),
-                                        outdoor_charts.user_ascent_chart(username),
-                                        outdoor_charts.user_ascent_pie_chart(username)
-                                        ],
-                    }
+                "userprofile": userprofile,
+                "chart_list": [
+                        indoor_charts.training_line_chart(username),
+                        indoor_charts.gym_pie_chart(username),
+                        outdoor_charts.user_ascent_chart(username),
+                        outdoor_charts.user_ascent_pie_chart(username)
+                        ],
+            }
 
         return render(request, 'accounts/userprofile_detail.html',
-                        context_dict)
+                      context_dict)
     else:
-        return render(request, 'accounts/userprofile_error.html', {'username':username})
+        return render(request, 'accounts/userprofile_error.html',
+                      {'username': username})
 
-# CRUD views
+
 class SignUp(CreateView):
     # account creation
     form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
+
 class CreateUserProfile(LoginRequiredMixin, CreateView):
     # profile page creation
     model = UserProfile
     fields = ['first_name', 'last_name', 'email',
-                'description', 'avatar', 'hidden']
+              'description', 'avatar', 'hidden']
 
     def form_valid(self, form):
-         self.object = form.save(commit=False)
-         self.object.user = self.request.user
-         self.success_url = reverse_lazy('accounts:user_profile',
-            kwargs={'username': self.object.user.username})
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.success_url = reverse_lazy(
+                        'accounts:user_profile', kwargs={
+                                'username': self.object.user.username})
 
-         self.object.save()
-         return super().form_valid(form)
+        self.object.save()
+        return super().form_valid(form)
+
 
 class UpdateUserProfile(LoginRequiredMixin, UpdateView):
     # profile page edit
     model = UserProfile
     fields = ['first_name', 'last_name', 'email',
-                'description', 'avatar', 'hidden']
+              'description', 'avatar', 'hidden']
     template_name = 'accounts/userprofile_update.html'
 
     def get_object(self, queryset=None):
         self.object = get_object_or_404(self.model, user=self.request.user)
-        self.success_url = reverse_lazy('accounts:user_profile',
-            kwargs={'username': self.object.user.username})
+        self.success_url = reverse_lazy(
+                'accounts:user_profile', kwargs={
+                        'username': self.object.user.username})
         return self.object
 
-###### Groups
+
 class CreateGroup(LoginRequiredMixin, CreateView):
     # creation of new user group
     fields = ('name', 'description')
     model = Group
 
+
 class SingleGroup(DetailView):
     # view od single user group
     model = Group
 
+
 class ListGroups(ListView):
     # list of all user groups
     model = Group
+
 
 class JoinGroup(LoginRequiredMixin, RedirectView):
     # joining existing group
@@ -120,6 +135,7 @@ class JoinGroup(LoginRequiredMixin, RedirectView):
             messages.success(self.request, 'You are now a member!')
         return super().get(request, *args, **kwargs)
 
+
 class LeaveGroup(LoginRequiredMixin, RedirectView):
     # leaving user group
     def get_redirect_url(self, *args, **kwargs):
@@ -130,7 +146,7 @@ class LeaveGroup(LoginRequiredMixin, RedirectView):
         try:
             membership = GroupMember.objects.filter(
                 user=self.request.user,
-                group__slug = self.kwargs.get('slug')
+                group__slug=self.kwargs.get('slug')
             ).get()
         except GroupMember.DoesNotExist:
             messages.warning(self.request, 'Sorry you are not in this group!')
